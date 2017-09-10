f6651107f7df5aaf0e183f5feb4b78467b770911
John Day <itisjohnday@gmail.com>
2017-09-10 20:07:56 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,50 @@
+# ‹def display_board(board)
+#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+#   puts "-----------"
+#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+#   puts "-----------"
+#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+# end
+# 
+# def input_to_index(input)
+#   index = input.to_i - 1
+#   return index
+# end
+# 
+# def valid_move?(board, index)
+#   if !position_taken?(board, index)
+#     if index.between?(0,8)
+#       true
+#     end
+#   end
+# end
+# 
+# def position_taken?(board, index)
+#   board[index] != " "
+# end
+# 
+# def move(board, index, token = 'X')
+#   if (valid_move?(board,index))
+#     board[index] = token
+#   else
+#     puts "Please enter a valid move"
+#   end
+#   return board
+# end
+# 
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
+# 
+#   if valid_move?(board,index)
+#     move(board,index)
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
+# end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,16 +53,29 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  index = input.to_i - 1
-  return index
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def valid_move?(board, index)
-  if !position_taken?(board, index)
-    if index.between?(0,8)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
       true
     end
+  end
+
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
   end
 end
 
@@ -23,24 +83,6 @@ def position_taken?(board, index)
   board[index] != " "
 end
 
-def move(board, index, token = 'X')
-  if (valid_move?(board,index))
-    board[index] = token
-  else
-    puts "Please enter a valid move"
-  end
-  return board
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-
-  if valid_move?(board,index)
-    move(board,index)
-    display_board(board)
-  else
-    turn(board)
-  end
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
