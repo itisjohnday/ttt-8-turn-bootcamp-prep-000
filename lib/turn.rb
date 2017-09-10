8f9a6f8995dd982fc085ae352b96662cf66bdac4
John Day <itisjohnday@gmail.com>
2017-09-10 18:55:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,18 @@ def input_to_index(input)
   index = input.to_i - 1
   return index
 end
+
+def valid_move?(board, index)
+  if (
+    return false
+  else 
+    return true
+end
+
+def position_take?(board, index)
+  if (board[index] == 'X' || board[index] == 'O')
+    return true
+  else 
+    return false
+  end
+end 
