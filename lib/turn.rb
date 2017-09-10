cea891514c53a0346ffd74cde606328ff84b7f30
John Day <itisjohnday@gmail.com>
2017-09-10 20:08:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,12 +5,12 @@
 #   puts "-----------"
 #   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 # end
-# 
+#
 # def input_to_index(input)
 #   index = input.to_i - 1
 #   return index
 # end
-# 
+#
 # def valid_move?(board, index)
 #   if !position_taken?(board, index)
 #     if index.between?(0,8)
@@ -18,11 +18,11 @@
 #     end
 #   end
 # end
-# 
+#
 # def position_taken?(board, index)
 #   board[index] != " "
 # end
-# 
+#
 # def move(board, index, token = 'X')
 #   if (valid_move?(board,index))
 #     board[index] = token
@@ -31,12 +31,12 @@
 #   end
 #   return board
 # end
-# 
+#
 # def turn(board)
 #   puts "Please enter 1-9:"
 #   input = gets.strip
 #   index = input_to_index(input)
-# 
+#
 #   if valid_move?(board,index)
 #     move(board,index)
 #     display_board(board)
